ec239d3f0a0473dbadbe7c585625124d1cf081f1
Erik <mortensenerikm@gmail.com>
2020-04-28 01:08:41 -0300

    Modificando Modelo User

--- app/User.php
+++ app/User.php
@@ -10,13 +10,25 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USER_VERIFIED = '1';
+    const USER_NO_VERIFIED = '0';
+
+    const USER_ADMIN = 'admin';
+    const USER_TEACHER = 'teacher';
+    const USER_STUDENT = 'student';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'role',
     ];
 
     /**
@@ -25,7 +37,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 
+        'remember_token',
+        'verification_token',
     ];
 
     /**
@@ -36,4 +50,25 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function isVerified(){
+        return $this->verified == User:: USER_VERIFIED;
+    }
+
+    public function isAdmin(){
+        return $this->role == User:: USER_ADMIN;
+    }
+
+    public function isTeacher(){
+        return $this->role == User:: USER_TEACHER;
+    }
+
+    public function isStudent(){
+        return $this->role == User:: USER_STUDENT;
+    }
+
+    public static function createVerificationToken(){
+        return str_random(30);
+    }
 }
